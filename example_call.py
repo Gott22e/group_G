c19ef5dd178832a554d4fe6e6132426102d26e4e
allison-nau <anau@bu.edu>
2021-04-30 02:04:03 -0400

    Update example_call.py
    Updated to accept a dictionary of strings and a dictionary of csvs

--- example_call.py
+++ example_call.py
@@ -4,7 +4,21 @@
 
 from data_import_charm import *
 
-study1 = ImportStudy(the_file="Phase 1 Sediment.csv", study_name="Test1", study_year=2005,
-                     sample_type="Sediment",
-                     geo_cord_system="unknown_A1", utm_cord_system="unknown_A2")
-study1.run_import()
+# Test with either Phase 1 Sediment (single text box) or
+# phase 3 sediment (with lab_results text box & locations text box)
+
+# To test on bioed without website:
+dict_of_strings = {}
+# Read in example strings (if not actually in website):
+with open("phase3_labresults.txt", "r", encoding='utf-8-sig') as my_file:
+    my_string = my_file.read()
+dict_of_strings["labresults"] = my_string
+with open("phase3_location.txt", "r", encoding='utf-8-sig') as my_file:
+    my_string = my_file.read()
+dict_of_strings["locations"] = my_string
+
+# This is the example call:
+string_study = ImportStudy(the_input=dict_of_strings, study_name="String FromBioed4", study_year=9999,
+                           sample_type="Sediment", geo_cord_system="Nonsense1", utm_cord_system="Nonsense3",
+                           is_dict_strings=True)
+string_study.run_import()
